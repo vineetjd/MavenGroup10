928e5ba30d1f9c621504c26aa10da3239c72d0e6
vineetjd <vineet839@gmail.com>
2018-11-16 14:50:28 +0530

    first commit

---
+++ src/main/java/com/controller/Add.java
@@ -0,0 +1,46 @@
+package com.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Add
+ */
+public class Add extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Add() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		int n1=Integer.parseInt(request.getParameter("n1"));
+		int n2=Integer.parseInt(request.getParameter("n2"));
+		int sum=n1+n2;
+		PrintWriter pw=response.getWriter();
+		pw.println("<h1>Sum is </h1>"+sum+".");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Add, mofos!</title>
+</head>
+<body>
+	<form action="Add">
+		<label for="n1">Number 1 </label>
+		<input type="number" id="n1" name="n1">
+		<label for="n2">Number 2 </label>
+		<input type="number" id="n2" name="n2">
+		<input type="submit" value="+">
+	</form>
+</body>
+</html>
